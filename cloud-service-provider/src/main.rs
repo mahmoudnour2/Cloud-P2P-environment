a65bc46a8f66a726a49833d7e435a48dfe3ca8e1
mahmoudnour2 <mahmoudnour@aucegypt.edu>
2024-10-26 10:23:27 +0300

    added cloud udp server

--- cloud-service-provider/src/main.rs
+++ cloud-service-provider/src/main.rs
@@ -10,31 +10,50 @@ use service_provider::ImageEncryptor;
 use std::fs::File;
 use std::io::{Read, Write};
 fn main() {
-    let (sender, receiver): (Sender<Vec<u8>>, Receiver<Vec<u8>>) = unbounded();
 
-    let key = ChaCha20Poly1305::generate_key(&mut OsRng);
-    let encryptor = ImageEncryptor::new(key.clone(), sender);
+    let socket = UdpSocket::bind("10.40.32.167:8080")?;
+    println!("UDP server listening on 10.40.32.167:8080");
 
-    //Hide the image using steganography
-    let steganographer = image_steganographer::ImageSteganographer::new(75, 10);
-    let mut image = steganographer.hide_image("carrier.png","secret.png","output.png").unwrap();
+    let mut buffer = [0u8; 1024];
+    
+    loop {
+        // Wait for an incoming message
+        let (size, src) = socket.recv_from(&mut buffer)?;
 
-    // Load the image data
-    let mut image_data = Vec::new();
-    image.read_to_end(&mut image_data).expect("Failed to read image file");
+        // Convert the received bytes to a string (assuming UTF-8 encoded text)
+        if let Ok(message) = str::from_utf8(&buffer[..size]) {
+            println!("Received from {}: {}", src, message);
 
-    thread::spawn(move || {
-        encryptor.encrypt_and_send(image_data);
-    });
+            // For now, we'll just echo the message back to the sender
+            socket.send_to(message.as_bytes(), src)?;
+        }
+    }
+//     let (sender, receiver): (Sender<Vec<u8>>, Receiver<Vec<u8>>) = unbounded();
 
-    let received_data = receiver.recv().unwrap();
-    println!("Received encrypted data: {:?}", received_data);
+//     let key = ChaCha20Poly1305::generate_key(&mut OsRng);
+//     let encryptor = ImageEncryptor::new(key.clone(), sender);
 
-    // Save the encrypted image
-    let mut encrypted_file = File::create("encrypted_image.enc").expect("Failed to create encrypted file");
-    encrypted_file.write_all(&received_data).expect("Failed to write encrypted data");
+//     //Hide the image using steganography
+//     let steganographer = image_steganographer::ImageSteganographer::new(75, 10);
+//     let mut image = steganographer.hide_image("carrier.png","secret.png","output.png").unwrap();
 
-    // Save the key
-    let mut key_file = File::create("encryption_key.key").expect("Failed to create key file");
-    key_file.write_all(key.as_slice()).expect("Failed to write key");
+//     // Load the image data
+//     let mut image_data = Vec::new();
+//     image.read_to_end(&mut image_data).expect("Failed to read image file");
+
+//     thread::spawn(move || {
+//         encryptor.encrypt_and_send(image_data);
+//     });
+
+//     let received_data = receiver.recv().unwrap();
+//     println!("Received encrypted data: {:?}", received_data);
+
+//     // Save the encrypted image
+//     let mut encrypted_file = File::create("encrypted_image.enc").expect("Failed to create encrypted file");
+//     encrypted_file.write_all(&received_data).expect("Failed to write encrypted data");
+
+//     // Save the key
+//     let mut key_file = File::create("encryption_key.key").expect("Failed to create key file");
+//     key_file.write_all(key.as_slice()).expect("Failed to write key");
+// 
 }
