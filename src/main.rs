165cd7f6d05a04d1d5108892bbf9b234df2b8cca
MagdElDinAhmed <magdeldin@aucegypt.edu>
2024-11-01 21:58:03 +0200

    Help

--- src/main.rs
+++ src/main.rs
@@ -3,194 +3,78 @@
 //! Checkout the `README.md` for guidance.
 
 use std::{
-    error::Error,
+    fs,
+    io::{self, Write},
     net::{IpAddr, Ipv4Addr, SocketAddr},
+    path::PathBuf,
     sync::Arc,
+    time::{Duration, Instant},
+    thread,
 };
 
+use anyhow::{anyhow, Result};
+use clap::Parser;
 use quinn_proto::crypto::rustls::QuicClientConfig;
-use quinn::{ClientConfig, Endpoint, ServerConfig};
 use rustls::pki_types::{CertificateDer, ServerName, UnixTime, PrivatePkcs8KeyDer};
+use tracing::{error, info};
+use url::Url;
+use quinn::{Endpoint, ClientConfig, ServerConfig};
+use std::error::Error;
+use std::net::ToSocketAddrs;
+use std::fs::File;
 
+mod client;
+mod server;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
-    // server and client are running on the same thread asynchronously
-    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 8080);
-    tokio::spawn(run_server(addr));
-    run_client(addr).await?;
-    // run_server(addr);
-    Ok(())
-}
-
-/// Runs a QUIC server bound to given address.
-async fn run_server(addr: SocketAddr) {
-    let (endpoint, _server_cert) = make_server_endpoint(addr).unwrap();
-    // accept a single connection
-    let incoming_conn = endpoint.accept().await.unwrap();
-    let conn = incoming_conn.await.unwrap();
-    println!(
-        "[server] connection accepted: addr={}",
-        conn.remote_address()
-    );
-}
-
-async fn run_client(server_addr: SocketAddr) -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
-    let mut endpoint = Endpoint::client(SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 0))?;
-
-    endpoint.set_default_client_config(ClientConfig::new(Arc::new(QuicClientConfig::try_from(
-        rustls::ClientConfig::builder()
-            .dangerous()
-            .with_custom_certificate_verifier(SkipServerVerification::new())
-            .with_no_client_auth(),
-    )?)));
-
-    // connect to server
-    let connection = endpoint
-        .connect(server_addr, "localhost")
-        .unwrap()
-        .await
-        .unwrap();
-    println!("[client] connected: addr={}", connection.remote_address());
-    // Dropping handles allows the corresponding objects to automatically shut down
-    drop(connection);
-    // Make sure the server has a chance to clean up
-    endpoint.wait_idle().await;
-
-    Ok(())
-}
-
-/// Dummy certificate verifier that treats any certificate as valid.
-/// NOTE, such verification is vulnerable to MITM attacks, but convenient for testing.
-#[derive(Debug)]
-struct SkipServerVerification(Arc<rustls::crypto::CryptoProvider>);
-
-impl SkipServerVerification {
-    fn new() -> Arc<Self> {
-        Arc::new(Self(Arc::new(rustls::crypto::ring::default_provider())))
-    }
-}
-
-impl rustls::client::danger::ServerCertVerifier for SkipServerVerification {
-    fn verify_server_cert(
-        &self,
-        _end_entity: &CertificateDer<'_>,
-        _intermediates: &[CertificateDer<'_>],
-        _server_name: &ServerName<'_>,
-        _ocsp: &[u8],
-        _now: UnixTime,
-    ) -> Result<rustls::client::danger::ServerCertVerified, rustls::Error> {
-        Ok(rustls::client::danger::ServerCertVerified::assertion())
-    }
-
-    fn verify_tls12_signature(
-        &self,
-        message: &[u8],
-        cert: &CertificateDer<'_>,
-        dss: &rustls::DigitallySignedStruct,
-    ) -> Result<rustls::client::danger::HandshakeSignatureValid, rustls::Error> {
-        rustls::crypto::verify_tls12_signature(
-            message,
-            cert,
-            dss,
-            &self.0.signature_verification_algorithms,
-        )
-    }
-
-    fn verify_tls13_signature(
-        &self,
-        message: &[u8],
-        cert: &CertificateDer<'_>,
-        dss: &rustls::DigitallySignedStruct,
-    ) -> Result<rustls::client::danger::HandshakeSignatureValid, rustls::Error> {
-        rustls::crypto::verify_tls13_signature(
-            message,
-            cert,
-            dss,
-            &self.0.signature_verification_algorithms,
-        )
-    }
-
-    fn supported_verify_schemes(&self) -> Vec<rustls::SignatureScheme> {
-        self.0.signature_verification_algorithms.supported_schemes()
-    }
-}
-
-
-pub fn make_client_endpoint(
-    bind_addr: SocketAddr,
-    server_certs: &[&[u8]],
-) -> Result<Endpoint, Box<dyn Error + Send + Sync + 'static>> {
-    let client_cfg = configure_client(s#[derive(Debug)]
-    struct SkipServerVerification(Arc<rustls::crypto::CryptoProvider>);
+use client::*;
+use server::*;
+fn main() {
     
-    impl SkipServerVerification {
-        fn new() -> Arc<Self> {
-            Arc::new(Self(Arc::new(rustls::crypto::ring::default_provider())))
+    let server_thread = thread::spawn(|| {
+        tracing::subscriber::set_global_default(
+        tracing_subscriber::FmtSubscriber::builder()
+            .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+            .finish(),
+    )
+    .unwrap();
+    let opt = server::Opt::parse();
+    let code = {
+        if let Err(e) = server::run(opt) {
+            eprintln!("ERROR: {e}");
+            1
+        } else {
+            0
         }
-    }
+    };
+    ::std::process::exit(code);
+    });
     
-    impl rustls::client::danger::ServerCertVerifier for SkipServerVerification {
-        fn verify_server_cert(
-            &self,
-            _end_entity: &CertificateDer<'_>,
-            _intermediates: &[CertificateDer<'_>],
-            _server_name: &ServerName<'_>,
-            _ocsp: &[u8],
-            _now: UnixTime,
-        ) -> Result<rustls::client::danger::ServerCertVerified, rustls::Error> {
-            Ok(rustls::client::danger::ServerCertVerified::assertion())
+    
+    let client_thread = thread::spawn(|| {
+        tracing::subscriber::set_global_default(
+        tracing_subscriber::FmtSubscriber::builder()
+            .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+            .finish(),
+    )
+    .unwrap();
+    let opt = client::Opt::parse();
+    // Check if the bind address is localhost
+    if opt.bind.ip().is_loopback() {
+        eprintln!("ERROR: Binding to localhost is not allowed.");
+        std::process::exit(1);
+    }
+    let code = {
+        if let Err(e) = client::run(opt) {
+            eprintln!("ERROR: {e}");
+            1
+        } else {
+            0
         }
-    
-        fn verify_tls12_sign
+    };
+    ::std::process::exit(code);
+    });
+
+    server_thread.join().unwrap();
+    client_thread.join().unwrap();
 }
 
-/// Constructs a QUIC endpoint configured to listen for incoming connections on a certain address
-/// and port.
-///
-/// ## Returns
-///
-/// - a stream of incoming QUIC connections
-/// - server certificate serialized into DER format
-
-pub fn make_server_endpoint(
-    bind_addr: SocketAddr,
-) -> Result<(Endpoint, CertificateDer<'static>), Box<dyn Error + Send + Sync + 'static>> {
-    let (server_config, server_cert) = configure_server()?;
-    let endpoint = Endpoint::server(server_config, bind_addr)?;
-    Ok((endpoint, server_cert))
-}
-
-/// Builds default quinn client config and trusts given certificates.
-///
-/// ## Args
-///
-/// - server_certs: a list of trusted certificates in DER format.
-fn configure_client(
-    server_certs: &[&[u8]],
-) -> Result<ClientConfig, Box<dyn Error + Send + Sync + 'static>> {
-    let mut certs = rustls::RootCertStore::empty();
-    for cert in server_certs {
-        certs.add(CertificateDer::from(*cert))?;
-    }
-
-    Ok(ClientConfig::with_root_certificates(Arc::new(certs))?)
-}
-
-/// Returns default server configuration along with its certificate.
-fn configure_server(
-) -> Result<(ServerConfig, CertificateDer<'static>), Box<dyn Error + Send + Sync + 'static>> {
-    let cert = rcgen::generate_simple_self_signed(vec!["localhost".into()]).unwrap();
-    let cert_der = CertificateDer::from(cert.cert);
-    let priv_key = PrivatePkcs8KeyDer::from(cert.key_pair.serialize_der());
-
-    let mut server_config =
-        ServerConfig::with_single_cert(vec![cert_der.clone()], priv_key.into())?;
-    let transport_config = Arc::get_mut(&mut server_config.transport).unwrap();
-    transport_config.max_concurrent_uni_streams(0_u8.into());
-
-    Ok((server_config, cert_der))
-}
-
-#[allow(unused)]
-pub const ALPN_QUIC_HTTP: &[&[u8]] = &[b"hq-29"];
