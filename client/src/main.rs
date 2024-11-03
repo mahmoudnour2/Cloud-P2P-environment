fb55f2887cc5b3c89afee4f7461af2eda186d875
MagdElDinAhmed <magdeldin@aucegypt.edu>
2024-11-03 08:48:30 +0200

    help meeeeeeeee

--- client/src/main.rs
+++ client/src/main.rs
@@ -21,7 +21,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         "10.7.16.71:5001".parse()?,
         "10.7.16.71:5002".parse()?
     ];  // Connect to server's ports
-    let client_addr: SocketAddr = "127.0.0.1:4800".parse()?;  // Listen on this port
+    let client_addr: SocketAddr = "10.7.16.80:4800".parse()?;  // Listen on this port
 
     println!("Quinn endpoints setup beginning.");
 
