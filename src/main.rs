480d26d12171c0e4fbb95a91032dd7b719a38bf8
ymentawy <mentawy@aucegypt.edu>
2024-11-23 22:37:27 +0200

    fixed writing issues

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,34 @@
-mod dos;  // This tells Rust to look for the dos.rs file
-
-use dos::{read_dos_file, write_dos_file, DosEntry, setup_drive_hub ,add_entry, delete_entry};
+mod dos;
+use dos::{read_dos_file, write_dos_file, DosEntry, setup_drive_hub, clean_entries};
 use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let hub = setup_drive_hub().await?;
-    let filename = "DoS.tsv";  // Replace with your actual file name
-    let dos_entries = read_dos_file(&hub, filename).await?;
+    let filename = "DoS.tsv";
+    
+    // Read current entries
+    let mut dos_entries = read_dos_file(&hub, filename).await?;
+    
+    // Clean existing entries
+    clean_entries(&mut dos_entries);
     
     // Add a new entry
-    let new_entry = DosEntry {
-        Client_ID: "new_client1".to_string(),
-        Client_IP: "192.168.0.2".to_string(),
-        resources: "resource_2".to_string(),
-    };
-    let mut updated_entries = dos_entries.clone();
-    add_entry(&mut updated_entries, new_entry);
-
-    // Delete an entry by Client_ID
-    delete_entry(&mut updated_entries, "1234");
+    let new_entry = DosEntry::new(
+        "new_client1".to_string(),
+        "192.168.0.2".to_string(),
+        "resource_2".to_string(),
+    );
+    let new_entry1 = DosEntry::new(
+        "new_client2".to_string(),
+        "192.168.0.4".to_string(),
+        "resource_r".to_string(),
+    );
+    dos_entries.push(new_entry);
+    dos_entries.push(new_entry1);
 
     // Write the updated entries back to Google Drive
-    write_dos_file(&hub, filename, &updated_entries, "DoS").await?;
+    write_dos_file(&hub, filename, &dos_entries, "DoS").await?;
 
     Ok(())
 }
